eb3adc1d40c44489a86bd1aa2d840701dd3defaa
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-02-05 23:17:34 +0100

    taking input value with enter key pressed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,19 @@
-$('li').click(function () {
-  $(this).toggleClass('completed')
+$('ul').on('click', 'li' ,function () {
+  $(this).toggleClass('completed');
 })
 
-$('span').click(function(e) {
+$('ul').on('click', 'span', function(e) {
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   e.stopPropagation();
+})
+
+$('input[type="text"]').keypress(function(e) {
+  if (e.which === 13) {
+    let todoText = $(this).val();
+    $('ul').append(`<li><span>X</span> ${todoText} </li>`);
+    $(this).val("");
+  }
+  console.log('keypressed');
 })
