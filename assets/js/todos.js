fca20f4e0ff917b0e860ae3e061d4ffda15242f8
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-02-06 01:55:27 +0100

    finshed the todo app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -12,8 +12,11 @@ $('ul').on('click', 'span', function(e) {
 $('input[type="text"]').keypress(function(e) {
   if (e.which === 13) {
     let todoText = $(this).val();
-    $('ul').append(`<li><span>X</span> ${todoText} </li>`);
+    $('ul').append(`<li><span><i class="fa fa-trash"></i></span> ${todoText} </li>`);
     $(this).val("");
   }
-  console.log('keypressed');
+});
+
+$('.fa-plus').click(function() {
+  $('input[type="text"]').fadeToggle();
 })
